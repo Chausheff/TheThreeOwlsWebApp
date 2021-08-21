bdb05bd6465b3459c3305c9da6147f32f3587a16
Stefan Chausheff <chausheff@gbg.bg>
2021-08-21 17:52:03 +0300

    Adding full support of the blog

--- TheThreeOwlsWebApp/Controllers/BlogController.cs
+++ TheThreeOwlsWebApp/Controllers/BlogController.cs
@@ -28,9 +28,83 @@
             return View(articles);
         }
 
+        public IActionResult Edit(string id)
+        {
+            var article = this.data.Articles
+              .Where(a => a.Id == id)
+              .Select(a => new BlogListingViewModel
+              {
+                  Id = a.Id,
+                  Image = a.Image,
+                  Title = a.Title,
+                  Text = a.Text
+              })
+             .FirstOrDefault();
+
+            if (article == null)
+            {
+                return NotFound();
+            }
+            return View(article);
+        }
+        [HttpPost]
+        public IActionResult Edit(BlogListingViewModel article)
+        {
+            var editedArticle = data.Articles.FirstOrDefault(a => a.Id == article.Id);
+
+            if (editedArticle == null)
+            {
+                return NotFound();
+            }
+
+            editedArticle.Image = article.Image;
+            editedArticle.Title = article.Title;
+            editedArticle.Text = article.Text;
+
+            this.data.Articles.Update(editedArticle);
+            this.data.SaveChanges();
+
+            return RedirectToAction("All", "Blog");
+        }
+
+        public IActionResult Delete(string id)
+        {
+            var removedArticle = this.data.Articles
+                .FirstOrDefault(a => a.Id == id);
+
+            if (removedArticle == null)
+            {
+                return NotFound();
+            }
+
+            this.data.Articles.Remove(removedArticle);
+            this.data.SaveChanges();
+            return RedirectToAction("All", "Blog");
+        }
+
         public IActionResult Add()
         {
             return View();
+        }
+
+        public IActionResult Details(string Id)
+        {
+            var article = this.data.Articles
+                .Where(a => a.Id == Id)
+                 .Select(a => new BlogListingViewModel
+                 {
+                    Id = a.Id,
+                    Image = a.Image,
+                    Title = a.Title,
+                    Text = a.Text
+                 })
+                 .FirstOrDefault();
+            if (article != null)
+            {
+                return View(article);
+            }
+
+            return NotFound();
         }
 
         [HttpPost]
