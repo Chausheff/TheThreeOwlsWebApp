6ca9771e0c38b185a091d3ce7c78c291f7941bd5
Stefan Chausheff <chausheff@gbg.bg>
2021-09-02 21:11:08 +0300

    Added StudyHall feature (Not finished)

---
+++ TheThreeOwlsWebApp/Controllers/StudyHall.cs
@@ -0,0 +1,22 @@
+﻿namespace TheThreeOwlsWebApp.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class StudyHall : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Summer()
+        {
+            return View();
+        }
+
+        public IActionResult Educational()
+        {
+            return View();
+        }
+    }
+}
